4d924bf1f8dbceb0619ec9ef5710aca4658c1ab9
nicholasstano <nicholasstano@gmail.com>
2020-02-03 11:34:03 -0500

    postgres

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 2020_01_27_153245) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "albums", force: :cascade do |t|
     t.string "name"
     t.string "artist"
