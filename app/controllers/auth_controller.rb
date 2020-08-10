8076f36f3a1dfcae2acb539cf8c2ee46dd7057e0
nicholasstano <nicholasstano@gmail.com>
2020-08-10 12:25:19 -0400

    updating jwt

--- app/controllers/auth_controller.rb
+++ app/controllers/auth_controller.rb
@@ -2,7 +2,7 @@ class AuthController < ApplicationController
     def login
         user = User.find_by(username: params[:username])
         if (user && user.authenticate(params[:password]))
-            render json: {user: user, token: JWT.encode({userId: user.id}, 'secret')}
+            render json: {user: user, token: JWT.encode({userId: user.id}, 'itsasecretmissioninunchartedspaceletsgo')}
         else
             render json: {errors: "invalid username"}
         end
@@ -10,7 +10,7 @@ class AuthController < ApplicationController
 
     def autologin
         token = request.headers['Authorization']
-        user_id = JWT.decode(token, 'secret')[0]["userId"]
+        user_id = JWT.decode(token, 'itsasecretmissioninunchartedspaceletsgo')[0]["userId"]
         user = User.find(user_id)
         render json: user
     end
